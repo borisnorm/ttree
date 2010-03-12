8204e7b673605762d8604b28d6cffb20b607d3c8
Dan Kruchinin <just.asgard@gmail.com>
2010-03-12 19:15:25 +0300

    Changed my email address in headers of all source files to a new one.

--- ttree.h
+++ ttree.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2009 Dan Kruchinin <dan.kruchinin@gmail.com>
+ * Copyright (c) 2008, 2009 Dan Kruchinin <dkruchinin@acm.org>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
